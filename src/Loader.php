7de62b6bb53ec4a5afc5528c82a9b4cdfe5c493e
Caio Marcatti <caio.marcatti@rb.com.br>
2023-02-18 09:06:32 -0300

    fix launcher environment test

--- src/Loader.php
+++ src/Loader.php
@@ -14,16 +14,16 @@ use CaioMarcatti12\Env\Objects\Property;
 use CaioMarcatti12\Mail\Adapter\MemoryAdapter;
 use CaioMarcatti12\Mail\Interfaces\MailerInterface;
 
-#[Launcher(LauncherPriorityEnum::BEFORE_LOAD_APPLICATION)]
+#[Launcher(LauncherPriorityEnum::BEFORE_LOAD_FRAMEWORK)]
 class Loader implements LauncherInterface
 {  
     public function handler(): void
     {
-        if(Modules::isEnabled(ModulesEnum::MAIL)){
-            if(Assert::equalsIgnoreCase(Property::get('application.env', 'test'), 'test')){
-                BeanProxy::add(MailerInterface::class, MemoryAdapter::class);
-            }
+        if(Assert::equalsIgnoreCase(Property::get('application.env', 'test'), 'test')){
+            BeanProxy::add(MailerInterface::class, MemoryAdapter::class);
+        }
 
+        if(Modules::isEnabled(ModulesEnum::MAIL)){
             InstanceFactory::createIfNotExists(MailerInterface::class);
         }
     }
